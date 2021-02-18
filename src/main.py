e21609df9add465c86d230826d86f34775160144
Nikolay Fedurko <kolausf@gmail.com>
2021-02-18 03:37:43 +0200

    fix: prohibited accepting requests from everyone

--- src/main.py
+++ src/main.py
@@ -12,7 +12,6 @@ load_dotenv(dotenv_path=Path('..') / '.env')
 
 origins = os.getenv('CORS_ALLOWED_HOSTS') 
 origins = [
-    '*',
     'http://localhost',
     'http://localhost:3000',
     *(origins.split(';') if origins is not None else [])
